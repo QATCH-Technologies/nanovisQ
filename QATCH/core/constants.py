1dad24a081f9405d83193b43c2dc31036c6eb1b6
Alexander J. Ross <alexander.ross@qatchtech.com>
2024-05-23 19:27:20 -0400

    Prep for tagging v2.6b36 Update Release Notes, documentation, and target FW build

--- QATCH/core/constants.py
+++ QATCH/core/constants.py
@@ -40,8 +40,8 @@ class Constants:
     ########################
     # RECOMMENDED firmware #
     ########################
-    # best_fw_version = "v2.6b32"
-    best_fw_version = app_version # may specify an exact version if needed
+    best_fw_version = "v2.6b32"
+    # best_fw_version = app_version # may specify an exact version if needed
     do_legacy_updates = False # only use on FW v2.5b23 or older; will break newer devices!
 
     ###########################
