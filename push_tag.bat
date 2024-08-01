379d370068ee4018fc71bd6546fecb3da94710bb
Alexander J. Ross <alexander.ross@qatchtech.com>
2024-08-01 14:55:50 -0400

    Tagging v2.6r45

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.6b45 -m "nanovisQ_SW_v2.6b45 (2024-08-01)"
-git push origin v2.6b45
+git tag -a v2.6r45 -m "nanovisQ_SW_v2.6r45 (2024-08-01)"
+git push origin v2.6r45
 git tag -l --sort=taggerdate > tags.txt
 REM move 'tags.txt' to 'dist' folder
 pause
