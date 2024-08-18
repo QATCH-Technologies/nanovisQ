c92c041b2963d248abc6339a10eb53c37a13e4d9
Alexander J. Ross <alexander.ross@qatchtech.com>
2024-08-18 13:06:51 -0400

    Prep for tagging v2.6r46

--- QATCH/core/constants.py
+++ QATCH/core/constants.py
@@ -38,7 +38,7 @@ class Constants:
     # APPLICATION parameters #
     ##########################
     app_title = "QATCH nanovisQ Real-Time GUI"
-    app_version = "v2.6b46"
+    app_version = "v2.6r46"
     app_date = "2024-08-18"
     app_sources = ["Calibration Qatch Q-1 Device", "Measurement Qatch Q-1 Device"]
     app_publisher = "QATCH"
