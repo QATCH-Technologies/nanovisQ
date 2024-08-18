9b04e7fc7c1a127bcfce8f149ca7af8bf957add0
Alexander J. Ross <alexander.ross@qatchtech.com>
2024-08-18 13:14:29 -0400

    Prep for tag v2.6r46

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.6b46 -m "nanovisQ_SW_v2.6b46 (2024-08-18)"
-git push origin v2.6b46
+git tag -a v2.6r46 -m "nanovisQ_SW_v2.6r46 (2024-08-18)"
+git push origin v2.6r46
 git tag -l --sort=taggerdate > tags.txt
 REM move 'tags.txt' to 'dist' folder
 pause
