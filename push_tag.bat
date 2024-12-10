b656b4672a93b7241c756a957c7326223252653a
Alexander J. Ross <alexander.ross@qatchtech.com>
2024-12-10 15:46:19 -0500

    Build artifacts from tag v2.6b50 on 12/6

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.6b49 -m "nanovisQ_SW_v2.6b49 (2024-11-08)"
-git push origin v2.6b49
+git tag -a v2.6b50 -m "nanovisQ_SW_v2.6b50 (2024-12-06)"
+git push origin v2.6b50
 git tag -l --sort=taggerdate > tags.txt
 REM move 'tags.txt' to 'dist' folder
 pause
