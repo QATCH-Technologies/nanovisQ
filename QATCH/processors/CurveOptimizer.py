ac72265fdcd5071dff5169b78b938127b8cf9fb7
paulm <paulmacnichol@gmail.com>
2025-01-23 09:29:12 -0500

    documenation and error checking

--- QATCH/processors/CurveOptimizer.py
+++ QATCH/processors/CurveOptimizer.py
@@ -3,39 +3,74 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from QATCH.common.logger import Logger as Log
+from QATCH.core.constants import Constants
 
 TAG = ["CurveOptimizer"]
 
+""" The percentage of the run data to ignore from the head of a difference curve. """
+HEAD_TRIM_PERCENTAGE = 0.05
+""" The percentage of the run data to ignore from the tail of a difference curve. """
+TAIL_TRIM_PERCENTAGE = 0.5
+""" Sets the left bound of the region behind ROI. """
+BASE_OFFSET = 0.005
+""" Restricts the difference factor. """
+DIFFERENCE_FACTOR_RESTRICTION = (1.0, 3.0)
+
 
 class CurveOptimizer:
-    def __init__(self, file_buffer, initial_diff_factor: float = 2.0):
-        self.file_buffer = file_buffer
-        self.initial_diff_factor = initial_diff_factor
+    def __init__(self, file_buffer, initial_diff_factor: float = Constants.default_diff_factor) -> None:
+        """
+        Initializes the optimizer utilities such as the data buffer, dataframe object, left and right ROI
+        bounds, and initial difference factor.
+
+        Given a file buffer and an initial difference factor (typically 2.0), the file buffer is initialized
+        to a data buffer and data is loaded into a dataframe from this buffer.  If an error occurs, an exception
+        is thrown at this point.  Initial difference curve is initialized to 'None' and and the left/right ROI
+        time/index bounds are initialized to -1.  Optimal difference factor is set to None.
+
+        Args:
+            file_buffer: location to load data from.
+            initial_diff_factor (float): initial difference factor to begin optimization at (Optional)
+
+        Returns:
+            None
+
+        Raises:
+            Exception if an error occurs during initailizing a file buffer.
+        """
+        self._file_buffer = file_buffer
+        self._initial_diff_factor = initial_diff_factor
         try:
-            self.filepath = self._initialize_file_buffer(file_buffer)
-            self.df = pd.read_csv(self.filepath)
-            Log.d(TAG, f"CSV file loaded successfully from {self.filepath}.")
+            self._data_buffer = self._initialize_file_buffer(file_buffer)
+            self._dataframe = pd.read_csv(self._data_buffer)
+            Log.d(
+                TAG, f"Run data loaded successfully from {self._data_buffer}.")
         except Exception as e:
-            Log.e(TAG, f"Failed to load CSV file: {e}")
+            Log.e(TAG, f"Failed to load data file: {e}")
             raise
 
-        self.curve = None
-        self.lb = {"time": -1, "index": -1}
-        self.rb = {"time": -1, "index": -1}
-        self.optimal_difference_factor = None
+        self._difference_curve = None
+        self._left_bound = {"time": -1, "index": -1}
+        self._right_bound = {"time": -1, "index": -1}
+        self._optimal_difference_factor = None
 
     def _initialize_file_buffer(self, file_buffer):
         """
-        Prepares the file buffer for reading.
+        Initializes the input file buffer.
+
+        Given a file buffer, this function checks if the file bufer has a sekable attribute
+        and the file_buffer is seakable.  If it is, this function sets the data buffer to 
+        the head of the seekable object.  Otherwise, the function raises a value error stating
+        the buffer is not seekable.  The file buffer is retruned on success.
 
         Args:
-            file_buffer: File-like object or file path.
+            file_buffer: the input data buffer to initailize
 
         Returns:
-            File-like object: Prepared file buffer.
+            file_buffer seekable object.
 
         Raises:
-            ValueError: If the file buffer is invalid.
+            ValueError: if the file buffer has not seek attribute or the object is not seekable.
         """
         Log.d(TAG, 'Initializing file buffer.')
         if not isinstance(file_buffer, str):
@@ -47,7 +82,23 @@ class CurveOptimizer:
         return file_buffer
 
     @staticmethod
-    def normalize(data: np.ndarray) -> np.ndarray:
+    def _normalize(data: np.ndarray) -> np.ndarray:
+        """
+        Utility function to normalize an np.ndarray object.
+
+        This function performs min-max normalization to normalize the input
+        array between 0 and 1.  If an error occurs durring normalization, the fuction raises
+        the error to the caller.
+
+        Args:
+            data (np.ndarray): The 1xN input array to normalize.
+
+        Returns:
+            np.ndarray: Input array normalized between 0 and 1.
+
+        Raises:
+            Exception to caller if normalization fails.
+        """
         try:
             min_val = np.min(data)
             max_val = np.max(data)
@@ -58,69 +109,116 @@ class CurveOptimizer:
             Log.e(TAG, f"Error normalizing data: {e}")
             raise
 
-    def generate_curve(self, difference_factor: float = None) -> np.ndarray:
+    def _generate_curve(self, difference_factor: float) -> np.ndarray:
+        """
+        Function to compute the difference curve with a parameterized difference factor.
+
+        Given a difference factor and a dataset containing Dissipation, Resonance_Frequency, and Relative_time
+        data as a pandas dataframe, the function attempts to compute the difference curve between the Dissipation
+        and Resonance_Frequency data using Relative_time as the X-Axis component.
+
+        Args:
+            difference_factor (float): A difference factor value used to compute the difference curve.
+
+        Returns:
+            np.ndarray: A 1xN array of difference curve Y values.
+
+        Raises:
+            ValueError: if the input dataframe does not contain the requeired columns "Dissipation", 
+                "Resonance_Frequency", and "Relative_time"
+        """
         try:
-            difference_factor = difference_factor or self.initial_diff_factor
+            difference_factor = difference_factor or self._initial_diff_factor
             required_columns = ["Dissipation",
                                 "Resonance_Frequency", "Relative_time"]
-            if not all(column in self.df.columns for column in required_columns):
+            if not all(column in self._dataframe.columns for column in required_columns):
                 Log.e(
                     TAG, f"Input CSV must contain the following columns: {required_columns}")
                 raise ValueError(
                     f"Input CSV must contain the following columns: {required_columns}")
 
-            xs = self.df["Relative_time"]
+            xs = self._dataframe["Relative_time"]
             i = next(x for x, t in enumerate(xs) if t > 0.5)
             j = next(x for x, t in enumerate(xs) if t > 2.5)
 
-            avg_resonance_frequency = self.df["Resonance_Frequency"][i:j].mean(
+            avg_resonance_frequency = self._dataframe["Resonance_Frequency"][i:j].mean(
             )
-            avg_dissipation = self.df["Dissipation"][i:j].mean()
+            avg_dissipation = self._dataframe["Dissipation"][i:j].mean()
 
-            self.df["ys_diss"] = (
-                self.df["Dissipation"] - avg_dissipation) * avg_resonance_frequency / 2
-            self.df["ys_freq"] = avg_resonance_frequency - \
-                self.df["Resonance_Frequency"]
-            self.df["Difference"] = self.df["ys_freq"] - \
-                difference_factor * self.df["ys_diss"]
+            self._dataframe["ys_diss"] = (
+                self._dataframe["Dissipation"] - avg_dissipation) * avg_resonance_frequency / 2
+            self._dataframe["ys_freq"] = avg_resonance_frequency - \
+                self._dataframe["Resonance_Frequency"]
+            self._dataframe["Difference"] = self._dataframe["ys_freq"] - \
+                difference_factor * self._dataframe["ys_diss"]
 
-            self.curve = np.stack(
-                (self.df["Relative_time"].values, self.df["Difference"].values), axis=1)
+            self._difference_curve = np.stack(
+                (self._dataframe["Relative_time"].values, self._dataframe["Difference"].values), axis=1)
             Log.d(TAG, "Curve generated successfully.")
-            return self.curve
+            return self._difference_curve
         except Exception as e:
             Log.e(TAG, f"Error generating curve: {e}")
             raise
 
-    def find_extreme(self, difference: np.ndarray, resonance_frequency: np.ndarray, dissipation: np.ndarray, time: pd.Series, mode: str = "top") -> float:
-        try:
-            ignore_index = int(len(difference) * 0.05)
-            ignore_index_2 = int(len(difference) * 0.5)
-            time = time.iloc[ignore_index:ignore_index_2]
-            difference = difference[ignore_index:ignore_index_2]
-            trend = np.linspace(0, difference.max(), len(difference))
-            adjusted_y = difference - trend
+    def _find_region(self, difference: np.ndarray, relative_time: pd.Series, mode: str) -> float:
+        """
+        Searches for the initial fill region to perform smoothness optimization at.
 
-            if mode == "base":
-                time = time.iloc[:self.rb["index"]]
-                difference = difference[:self.rb["index"]]
+        Given a difference data as a 1xN dataset, a relative time series as a 1xN dataset and a mode of operation
+        ('right' or 'left'), this function attempts to dynamically search for the initail fill region to optimize 
+        the smootheness of.  First, 5% of the head of the difference and relative time data is trimmed off followed 
+        by the last 50% of the difference and relative time data.  This avoids any unecessary noise in the run data 
+        as well as unimportant data after the initial fill region. Next, a trend is established between the maximal
+        difference value and the length of the run.  This trend is applied to the difference data to down trend the
+        entire dataset making the initial fill peak more prominent over the baseline or any later data.
+
+        Operating in 'left' mode requires a right bound be already established, the left bound is established as follows: 
+        Difference data and relative time data is trimmed on the right side again by the right bound.  The trend is then
+        re-established on the trimmed data and applied to the already adjusted difference curve to make the base of the
+        initial fill region more prominent.  The left-bound is returned as the global minima of this adjusted data stepped
+        back by a small offset (0.5% of the remaining data) to make sure that the entire initial fill trough is captured.
+
+        Operating in 'right' mode sets the right bound as follows: The global maxima of the adjusted difference factor is 
+        returned as the right bound of this data set.
+
+        Args:
+            difference (np.ndarray): A 1xN array containing 'Difference' data.
+            relative_time (pd.Series): A 1xN pandas series containing 'Relative_time' data.
+        """
+        try:
+            head_trim = int(len(difference) * HEAD_TRIM_PERCENTAGE)
+            tail_trim = int(len(difference) * TAIL_TRIM_PERCENTAGE)
+            relative_time = relative_time.iloc[head_trim:tail_trim]
+            difference = difference[head_trim:tail_trim]
+            trend = np.linspace(0, difference.max(), len(difference))
+            adjusted_difference = difference - trend
+
+            if mode == "left":
+                relative_time = relative_time.iloc[:self._right_bound["index"]]
+                difference = difference[:self._right_bound["index"]]
                 trend = np.linspace(0, difference.max(), len(difference))
-                adjusted_y = difference - trend
-                index = int(np.argmin(adjusted_y) - (len(time) * 0.005))
-                Log.d(TAG, f"Base extreme found at time: {time.iloc[index]}.")
-                return time.iloc[index], index
+                adjusted_difference = difference - trend
+                index = int(np.argmin(adjusted_difference) -
+                            (len(relative_time) * BASE_OFFSET))
+                Log.d(
+                    TAG, f"Left bound found at time: {relative_time.iloc[index]}.")
+                return relative_time.iloc[index], index
+            elif mode == 'right':
+                index = np.argmax(adjusted_difference)
+                Log.d(
+                    TAG, f"Right bound found at time: {relative_time.iloc[index]}.")
+                return relative_time.iloc[index], index
             else:
-                index = np.argmax(adjusted_y)
-                Log.d(TAG, f"Top extreme found at time: {time.iloc[index]}.")
-                return time.iloc[index], index
+                raise ValueError(f"Invalid search bound requested {mode}.")
         except Exception as e:
-            Log.e(TAG, f"Error finding extreme: {e}")
+            Log.e(TAG, f"Error finding optimization region: {e}")
             raise
 
-    def smoothness_objective(self, difference_factor: float, lb: float, rb: float) -> float:
+    def _objective(self, difference_factor: float, left_bound: float, right_bound: float) -> float:
         try:
-            curve = self.generate_curve(difference_factor)
-            region_mask = (curve[:, 0] >= lb) & (curve[:, 0] <= rb)
+            curve = self._generate_curve(difference_factor)
+            region_mask = (curve[:, 0] >= left_bound) & (
+                curve[:, 0] <= right_bound)
             differences = curve[region_mask, 1]
             time_points = curve[region_mask, 0]
 
@@ -134,41 +232,53 @@ class CurveOptimizer:
             Log.e(TAG, f"Error calculating smoothness objective: {e}")
             raise
 
-    def optimize_difference_factor(self) -> float:
+    def _optimize_difference_factor(self) -> float:
         try:
-            self.generate_curve(self.initial_diff_factor)
+            self._generate_curve(self._initial_diff_factor)
 
-            rb_time, rb_idx = self.find_extreme(
-                self.df["Difference"].values, self.df["Resonance_Frequency"].values, self.df["Dissipation"].values, self.df["Relative_time"], mode="top")
-            self.rb["time"] = rb_time
-            self.rb["index"] = rb_idx
+            rb_time, rb_idx = self._find_region(
+                self._dataframe["Difference"].values, self._dataframe["Resonance_Frequency"].values, self._dataframe["Dissipation"].values, self._dataframe["Relative_time"], mode="left")
+            self._right_bound["time"] = rb_time
+            self._right_bound["index"] = rb_idx
 
-            lb_time, lb_idx = self.find_extreme(
-                self.df["Difference"].values, self.df["Resonance_Frequency"].values, self.df["Dissipation"].values, self.df["Relative_time"], mode="base")
-            self.lb["time"] = lb_time
-            self.lb["index"] = lb_idx
+            lb_time, lb_idx = self._find_region(
+                self._dataframe["Difference"].values, self._dataframe["Resonance_Frequency"].values, self._dataframe["Dissipation"].values, self._dataframe["Relative_time"], mode="right")
+            self._left_bound["time"] = lb_time
+            self._left_bound["index"] = lb_idx
 
             result = minimize(
-                self.smoothness_objective,
-                self.initial_diff_factor,
-                args=(self.lb['time'], self.rb['time']),
+                self._objective,
+                self._initial_diff_factor,
+                args=(self._left_bound['time'], self._right_bound['time']),
                 method="Nelder-Mead"
             )
 
-            self.optimal_difference_factor = result.x[0]
-            Log.d(
-                TAG, f"Optimal difference factor: {self.optimal_difference_factor}")
-            return self.optimal_difference_factor
+            optimal_difference_factor = result.x[0]
+            if optimal_difference_factor < DIFFERENCE_FACTOR_RESTRICTION[0] or optimal_difference_factor > DIFFERENCE_FACTOR_RESTRICTION[1]:
+                Log.d(
+                    TAG, f"Optimal difference factor {self._optimal_difference_factor} out of range {DIFFERENCE_FACTOR_RESTRICTION}, using default.")
+                self._optimal_difference_factor = Constants.default_diff_factor
+            else:
+                Log.d(
+                    TAG, f"Optimal difference factor: {self._optimal_difference_factor}")
+                self._optimal_difference_factor = optimal_difference_factor
+
+            return self._optimal_difference_factor
         except Exception as e:
             Log.e(TAG, f"Error optimizing difference factor: {e}")
             raise
 
     def run(self):
         try:
-            result = self.optimize_difference_factor()
-            Log.d(
-                TAG, f"Run completed successfully. Results: {result}, lb: {self.lb['time']}, rb: {self.rb['time']}.")
-            return result, self.lb['time'], self.rb['time']
+            result = self._optimize_difference_factor()
+            if result:
+                Log.d(
+                    TAG, f"Run completed successfully. Results: {result}, left-bound: {self._left_bound['time']}, right-bound: {self._right_bound['time']}.")
+                return result, self._left_bound['time'], self._right_bound['time']
+            else:
+                Log.d(
+                    TAG, f"Run completed successfully. Results: {result}, left-bound: {self._left_bound['time']}, right-bound: {self._right_bound['time']}.")
+                return Constants.default_diff_factor, self._left_bound['time'], self._right_bound['time']
         except Exception as e:
             Log.e(TAG, f"Run failed: {e}")
             raise
