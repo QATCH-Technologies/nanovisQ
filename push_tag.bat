e0ca5751764ce57cd3b5b576e5494fcabef46422
Alexander J. Ross <alexander.ross@qatchtech.com>
2025-02-03 11:50:54 -0500

    Artifacts from tagging v2.6b51

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.6b50 -m "nanovisQ_SW_v2.6b50 (2024-12-06)"
-git push origin v2.6b50
+git tag -a v2.6b51 -m "nanovisQ_SW_v2.6b51 (2024-12-23)"
+git push origin v2.6b51
 git tag -l --sort=taggerdate > tags.txt
 REM move 'tags.txt' to 'dist' folder
 pause
