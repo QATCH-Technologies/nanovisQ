53387fccc3fe0e2a7d44b59c7ab86f4aa329dde3
Alexander J. Ross <alexander.ross@qatchtech.com>
2025-03-14 11:57:26 -0400

    other changed file too

--- QATCH/ui/mainWindow.py
+++ QATCH/ui/mainWindow.py
@@ -3314,24 +3314,25 @@ class MainWindow(QtWidgets.QMainWindow):
                     if self._get_source() == OperationType.measurement:
                         _ymin = np.amin(vector1)
                         _ymax = np.amax(vector1)
-                        if _ymax - _ymin < Constants.plot_min_range_freq:
+                        _yrange = _ymax - _ymin
+                        if _yrange < Constants.plot_min_range_freq:
                             _yavg = int(np.average(vector1))
                             _ymin = _yavg - (Constants.plot_min_range_freq / 2)
                             _ymax = _yavg + (Constants.plot_min_range_freq / 2)
 
+                        # Apply padding to _plt2 range to match that of _plt3
+                        _ymin -= Constants.default_plot_padding * _yrange
+                        _ymax += Constants.default_plot_padding * _yrange
+
                         _plt2.setLimits(
-                            yMax=_ymax, yMin=_ymin, minYRange=Constants.plot_min_range_freq, maxYRange=10000)
+                            yMax=_ymax, yMin=_ymin, minYRange=Constants.plot_min_range_freq)  # remove maxYRange limit
                         _plt3.setLimits(yMax=(
                             self._readFREQ[-1]-self._readFREQ[0])/self._readFREQ[0], yMin=0, minYRange=Constants.plot_min_range_diss)
 
-                        # Enable autorange with padding on _plt2 x and y axis of 10% of the data range.  Can be decreased
-                        # to any floating percentage of the available data range.
-                        _plt2.enableAutoRange(
-                            axis='x', enable=True, defaultPadding=Constants.default_plot_padding)
+                        _plt2.enableAutoRange(axis='x', enable=True)
                         _plt3.enableAutoRange(axis='x', enable=True)
 
-                        _plt2.enableAutoRange(
-                            axis='y', enable=True, defaultPadding=Constants.default_plot_padding)
+                        _plt2.enableAutoRange(axis='y', enable=True)
                         _plt3.enableAutoRange(axis='y', enable=True)
 
                     _plt3.addItem(pg.PlotCurveItem(self.worker.get_t2_buffer(i)[
