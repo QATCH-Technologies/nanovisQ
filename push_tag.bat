045d40bb1eddda20693e8b19edbe107665f83aa9
ajross4 <ajross4@alumni.ncsu.edu>
2025-05-16 15:04:24 -0400

    Update build artifacts

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.6b59 -m "nanovisQ_SW_v2.6b59 (2025-05-01)"
-git push origin v2.6b59
+git tag -a v2.6b60 -m "nanovisQ_SW_v2.6b60 (2025-05-16)"
+git push origin v2.6b60
 git tag -l --sort=taggerdate > tags.txt
 REM move 'tags.txt' to 'dist' folder
 pause
