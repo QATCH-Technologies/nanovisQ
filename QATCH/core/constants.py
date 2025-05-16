a09824c00e9071db87ac51758ee485e2f71c1128
ajross4 <ajross4@alumni.ncsu.edu>
2025-05-16 15:06:51 -0400

    Update release notes, prep for r tag

--- QATCH/core/constants.py
+++ QATCH/core/constants.py
@@ -41,7 +41,7 @@ class Constants:
     # APPLICATION parameters #
     ##########################
     app_title = "QATCH nanovisQ Real-Time GUI"
-    app_version = "v2.6b60"
+    app_version = "v2.6r60"
     app_date = "2025-05-16"
     app_sources = ["Calibration Qatch Q-1 Device",
                    "Measurement Qatch Q-1 Device"]
@@ -52,7 +52,7 @@ class Constants:
     ########################
     # RECOMMENDED firmware #
     ########################
-    best_fw_version = "v2.6b60"
+    best_fw_version = "v2.6r60"
     # best_fw_version = app_version # may specify an exact version if needed
     do_legacy_updates = False  # only use on FW v2.5b23 or older; will break newer devices!
 
