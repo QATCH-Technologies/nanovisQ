1bae3dc870ef19da9640d442c2c9a8a19cf62454
ajross4 <ajross4@alumni.ncsu.edu>
2025-05-19 18:32:15 -0400

    Exclude `QATCH.nightly` from SPEC compiled EXE Updating build artifacts for v2.6r60 recompiled

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.6b60 -m "nanovisQ_SW_v2.6b60 (2025-05-16)"
-git push origin v2.6b60
+git tag -a v2.6r60 -m "nanovisQ_SW_v2.6r60 (2025-05-19)"
+git push origin v2.6r60
 git tag -l --sort=taggerdate > tags.txt
 REM move 'tags.txt' to 'dist' folder
 pause
