a308bbcb1bf04625aba477ecbfb67526f972f6ce
ajross4 <ajross4@alumni.ncsu.edu>
2025-09-12 12:20:44 -0400

    Update version info, prep for tag

--- QATCH/core/constants.py
+++ QATCH/core/constants.py
@@ -41,8 +41,8 @@ class Constants:
     # APPLICATION parameters #
     ##########################
     app_title = "QATCH nanovisQ Real-Time GUI"
-    app_version = "v2.6b61"
-    app_date = "2025-07-02"
+    app_version = "v2.6b62"
+    app_date = "2025-09-12"
     app_sources = ["Calibration Qatch Q-1 Device",
                    "Measurement Qatch Q-1 Device"]
     app_publisher = "QATCH"
