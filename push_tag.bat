1be3774a3a3d5fcf926bb8c336dd782b25e1c6f1
ajross4 <ajross4@alumni.ncsu.edu>
2025-09-12 15:01:33 -0400

    Tagging nanovisQ_SW_v2.6b62 (2025-09-12)

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.6b61 -m "nanovisQ_SW_v2.6b61 (2025-07-02)"
-git push origin v2.6b61
+git tag -a v2.6b62 -m "nanovisQ_SW_v2.6b62 (2025-09-12)"
+git push origin v2.6b62
 git tag -l --sort=taggerdate > tags.txt
 REM move 'tags.txt' to 'dist' folder
 pause
