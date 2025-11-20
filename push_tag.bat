f0c84d0c30849514e24d9c24b780b1b0ee1a8afc
ajross4 <ajross4@alumni.ncsu.edu>
2025-11-20 14:06:21 -0500

    Final build artifacts for v2.6r66

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,6 @@
-git tag -a v2.6r64 -m "nanovisQ_SW_v2.6r64 (2025-10-13)"
-git push origin v2.6r64
+git tag -a v2.6r66 -m "nanovisQ_SW_v2.6r66 (2025-11-20)"
+git push origin v2.6r66
 git tag -l --sort=taggerdate > tags.txt
+call filter_yanked_tags
 REM move 'tags.txt' to 'dist' folder
 pause
