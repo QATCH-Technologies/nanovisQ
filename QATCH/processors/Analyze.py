Fix 317: Erroneous Difference Curve Inversion Logic Triggering Enhance inversion logic for difference curve by using absolute values for comparison of the constituent averages

QATCH/processors/Analyze.py
@@ -295,7 +295,7 @@ class AnalyzeProcess(QtWidgets.QWidget):
                     )  # NOTE: For temporary testing as of Pi Day 2023! (3 places in this file)
 
                     # Invert difference curve if drop applied to outlet
-                    if np.average(ys_diff) < 0:
+                    if np.average(np.abs(ys_freq)) < np.average(np.abs(diff_factor * ys)):
                         Log.w(
                             "Inverting DIFFERENCE curve due to negative initial fill deltas"
                         )
@@ -5160,7 +5160,7 @@ class AnalyzeProcess(QtWidgets.QWidget):
             ys_diff = ys_freq - (diff_factor * ys)
 
             # Invert difference curve if drop applied to outlet
-            if np.average(ys_diff) < 0:
+            if np.average(np.abs(ys_freq)) < np.average(np.abs(diff_factor * ys)):
                 Log.w("Inverting DIFFERENCE curve due to negative initial fill deltas")
                 ys_diff *= -1
 
@@ -6264,7 +6264,7 @@ class AnalyzerWorker(QtCore.QObject):
             ys_diff = ys_freq - (diff_factor * ys)
 
             # Invert difference curve if drop applied to outlet
-            if np.average(ys_diff) < 0:
+            if np.average(np.abs(ys_freq)) < np.average(np.abs(diff_factor * ys)):
                 Log.w("Inverting DIFFERENCE curve due to negative initial fill deltas")
                 ys_diff *= -1
 