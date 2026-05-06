7652d6ed7ac20a0b96c49b5e56f81ed5a93719f0
ajross4 <ajross4@alumni.ncsu.edu>
2026-05-06 15:25:19 -0400

    feat: update tag to v2.7r1 and add to tags list

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.6r69 -m "nanovisQ_SW_v2.6r69 (2026-02-24)"
-git push origin v2.6r69
+git tag -a v2.7r1 -m "nanovisQ_SW_v2.7r1 (2026-05-06)"
+git push origin v2.7r1
 git tag -l --sort=taggerdate > tags.txt
 call filter_yanked_tags
 REM move 'tags.txt' to 'dist' folder
