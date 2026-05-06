69fbddec6f5da519150671fa0ee9fdf57bec4e4c
ajross4 <ajross4@alumni.ncsu.edu>
2026-05-06 19:36:26 -0400

    Prep for tag v2.7b1

--- QATCH/core/constants.py
+++ QATCH/core/constants.py
@@ -42,7 +42,7 @@ class Constants:
     # APPLICATION parameters #
     ##########################
     app_title = "QATCH nanovisQ Real-Time GUI"
-    app_version = "v2.7r1"
+    app_version = "v2.7b1"
     app_date = "2026-05-06"
     app_sources = ["Calibration Qatch Q-1 Device", "Measurement Qatch Q-1 Device"]
     app_publisher = "QATCH"
--- QATCH_Q-1_FW_py_dev/src/main.cpp
+++ QATCH_Q-1_FW_py_dev/src/main.cpp
@@ -54,7 +54,7 @@
 
 // Build Info can be queried serially using command: "VERSION"
 #define DEVICE_BUILD "QATCH Q-1"
-#define CODE_VERSION "v2.7r1"
+#define CODE_VERSION "v2.7b1"
 #define RELEASE_DATE "2026-05-06"
 
 /************************** LIBRARIES **************************/
