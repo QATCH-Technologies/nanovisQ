f4b2ac2472d3adaab1e6c752cde52caad8cc2f95
ajross4 <ajross4@alumni.ncsu.edu>
2026-07-09 09:32:19 -0400

    tag prep: FW to v2.7b5 (2026-07-09)

--- QATCH_Q-1_FW_py_dev/src/main.cpp
+++ QATCH_Q-1_FW_py_dev/src/main.cpp
@@ -55,7 +55,7 @@
 // Build Info can be queried serially using command: "VERSION"
 #define DEVICE_BUILD "QATCH Q-1"
 #define CODE_VERSION "v2.7b5"
-#define RELEASE_DATE "2026-05-22"
+#define RELEASE_DATE "2026-07-09"
 
 /************************** LIBRARIES **************************/
 
