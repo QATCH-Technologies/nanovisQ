3ab0cf19fed2338c6a45126d14791073cda00d7f
ajross4 <ajross4@alumni.ncsu.edu>
2026-07-09 10:37:56 -0400

    post-tag: Update tags list

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.7r4 -m "nanovisQ_SW_v2.7r4 (2026-05-22)"
-git push origin v2.7r4
+git tag -a v2.7b5 -m "nanovisQ_SW_v2.7b5 (2026-07-09)"
+git push origin v2.7b5
 git tag -l --sort=taggerdate > tags.txt
 call filter_yanked_tags
 REM move 'tags.txt' to 'dist' folder
