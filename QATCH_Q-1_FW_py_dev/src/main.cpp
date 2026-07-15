198011669a0145ab186f147e3947844932f7b757
ajross4 <ajross4@alumni.ncsu.edu>
2026-07-15 13:14:11 -0400

    tag prep: App versions to v2.7r5 (2026-07-15)

--- QATCH_Q-1_FW_py_dev/src/main.cpp
+++ QATCH_Q-1_FW_py_dev/src/main.cpp
@@ -54,7 +54,7 @@
 
 // Build Info can be queried serially using command: "VERSION"
 #define DEVICE_BUILD "QATCH Q-1"
-#define CODE_VERSION "v2.7b6"
+#define CODE_VERSION "v2.7r6"
 #define RELEASE_DATE "2026-07-15"
 
 /************************** LIBRARIES **************************/
