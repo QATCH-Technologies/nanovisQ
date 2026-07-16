666b6a163ba10ec9ea2ce2c3a43969f9999fb711
ajross4 <ajross4@alumni.ncsu.edu>
2026-07-16 11:40:07 -0400

    tag prep: Update SPEC to hide Console window w/ hotfix hotfix: show calculators in R builds

--- push_tag.bat
+++ push_tag.bat
@@ -1,4 +1,4 @@
-git tag -a v2.7r6 -m "nanovisQ_SW_v2.7r6 (2026-07-15)"
+git tag -a v2.7r6 -m "nanovisQ_SW_v2.7r6 (2026-07-16)"
 git push origin v2.7r6
 git tag -l --sort=taggerdate > tags.txt
 call filter_yanked_tags
