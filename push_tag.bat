85f990ec08333727e36def19713f247e526d600e
ajross4 <ajross4@alumni.ncsu.edu>
2026-09-08 13:40:00 -0400

    tag: v2.7b8 post-tag artifacts

--- push_tag.bat
+++ push_tag.bat
@@ -1,5 +1,5 @@
-git tag -a v2.7r8 -m "nanovisQ_SW_v2.7r8 (2026-09-08)"
-git push origin v2.7r8
+git tag -a v2.7b8 -m "nanovisQ_SW_v2.7b8 (2026-09-08)"
+git push origin v2.7b8
 git tag -l --sort=taggerdate > tags.txt
 call filter_yanked_tags
 REM move 'tags.txt' to 'dist' folder
